d7235da3f9e8524e9beec96d7c266a7792ebb4be
Damikers <154735956+Damikers@users.noreply.github.com>
2024-10-30 17:44:29 +0000

    Remove [i]
    Оскільки ми прибрали for i, i++, приберемо індекси (можна було одним комітом)

--- Lab3/GildedRose.java
+++ Lab3/GildedRose.java
@@ -9,51 +9,51 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+            if (!items.name.equals("Aged Brie")
+                    && !items.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items].quality > 0) {
+                    if (!items.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        items.quality = items.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (items.quality < 50) {
+                    items.quality = items.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (items.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items.sellIn < 11) {
+                            if (items.quality < 50) {
+                                items.quality = items.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (items.sellIn < 6) {
+                            if (items.quality < 50) {
+                                items.quality = items.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!items.name.equals("Sulfuras, Hand of Ragnaros")) {
+                items.sellIn = items.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+            if (items.sellIn < 0) {
+                if (!items.name.equals("Aged Brie")) {
+                    if (!items.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (items.quality > 0) {
+                            if (!items.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                items.quality = items.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = 0;
+                        items.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (items.quality < 50) {
+                        items.quality = items.quality + 1;
                     }
                 }
             }
