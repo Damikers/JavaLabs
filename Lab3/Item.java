40be433f02fd5af90a10f75acb45e57dacc969b7
Damikers <154735956+Damikers@users.noreply.github.com>
2024-10-31 16:34:46 +0000

    Move decrease method
    Переносимо новий метод віднімання у клас Item для кращої структурованості

--- Lab3/Item.java
+++ Lab3/Item.java
@@ -21,7 +21,13 @@ public class Item {
 
     public void increaseQualityByOne() {
         if (this.quality < 50) {
-            this.quality = this.quality + 1;
+            this.quality ++;
+        }
+    }
+
+    public void decreaseQualityByOne() {
+        if (this.quality > 0) {
+            this.quality--;
         }
     }
 }
