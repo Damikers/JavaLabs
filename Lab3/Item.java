1835312472eaae8d1324e31366f9c7a70792a312
Damikers <154735956+Damikers@users.noreply.github.com>
2024-11-01 16:57:39 +0000

    Update Item.java
    Переробимо клас Item під додані нові 3 класи

--- Lab3/Item.java
+++ Lab3/Item.java
@@ -1,33 +1,47 @@
-// package com.gildedrose;
+package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private final ItemName name;
+    private final SellIn sellIn;
+    private final Quality quality;
 
-    public int sellIn;
-
-    public int quality;
-
-    public Item(String name, int sellIn, int quality) {
+    public Item(ItemName name, SellIn sellIn, Quality quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    @Override
+    public String toString() {
+        return name.nameIs() + ", " + sellIn.sellInIs() + ", " + quality.qualityIs();
     }
 
-    public void increaseQualityByOne() {
-        if (this.quality < 50) {
-            this.quality ++;
-        }
+    public void decreaseQuality() {
+        quality.decreaseQualityByOne();
     }
 
-    public void decreaseQualityByOne() {
-        if (this.quality > 0) {
-            this.quality--;
-        }
+    public void increaseQuality() {
+        quality.increaseQualityByOne();
+    }
+
+    public void resetQuality() {
+        quality.resetQuality();
+    }
+
+    public void decreaseSellIn() {
+        sellIn.decreaseSellInByOne();
+    }
+
+    public boolean isSellIn() {
+        return sellIn.isSellIn();
+    }
+
+    public boolean sellInValueLessThan(int value) {
+        return sellIn.sellInIs() < value;
+    }
+
+    public boolean isNamed(String itemName) {
+        return name.nameIs(itemName);
     }
 }
